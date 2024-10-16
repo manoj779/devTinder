50d4677387133e077511ef5d9f69c74e89d730a1
Manoj <manojsoft11@gmail.com>
2024-10-16 13:16:00 +0530

    commited changes of api

--- src/app.js
+++ src/app.js
@@ -36,6 +36,63 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/users", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const userData = await User.findOne({ emailId: userEmail });
+    res.send(userData);
+  } catch (err) {
+    res.status(400).send("Error while fetch users.." + err.message);
+  }
+});
+
+app.delete("/users", async (req, res) => {
+  const userId = req.body._id;
+  try {
+    await User.findByIdAndDelete(userId);
+    res.send("user: " + userId + "Deleted..!");
+  } catch (err) {
+    res.status(400).send("Deletion failed..!");
+  }
+});
+
+app.patch("/users/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATES = [
+      "firstName",
+      "lastName",
+      "password",
+      "gender",
+      "skills",
+      "age",
+      "about",
+    ];
+
+    const isAllowed = Object.keys(data).every((k) => {
+      const isValid = ALLOWED_UPDATES.includes(k);
+      console.log(`Key: ${k}, Is Valid: ${isValid}`);
+      return isValid;
+    });
+    if (!isAllowed) {
+      throw new Error("update is not allowed..");
+    }
+    if (data.skills.length > 3) {
+      throw new Error("Skills should not more than 3");
+    }
+    const result = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidator: true,
+    });
+    console.log(result);
+    res.send("User updated successfully..");
+  } catch (err) {
+    res.status(400).send("update failed: " + err.message);
+  }
+});
+
 connectDb()
   .then(() => {
     console.log("MongoDb connection is successful..!");
--- src/model/user.js
+++ src/model/user.js
@@ -1,25 +1,48 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default: "Image URL",
+    },
+    about: {
+      type: String,
+      default: "This is default about of the user",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
